50d6a09856919755774331529428cb5cb18c9daf
Rafael Matos <124383453+rafarito@users.noreply.github.com>
2025-02-10 16:14:02 -0300

    initial commit

---
+++ app/src/main/java/br/edu/ifba/inf008/shell/Core.java
@@ -0,0 +1,36 @@
+package br.edu.ifba.inf008.shell;
+
+import br.edu.ifba.inf008.interfaces.*;
+
+public class Core extends ICore
+{
+    private Core() {}
+
+    public static boolean init() {
+	if (instance != null) {
+	    System.out.println("Fatal error: core is already initialized!");
+	    System.exit(-1);
+	}
+
+	instance = new Core();
+        UIController.launch(UIController.class);
+
+        return true;
+    }
+    public IUIController getUIController() {
+        return UIController.getInstance();
+    }
+    public IAuthenticationController getAuthenticationController() {
+        return authenticationController;
+    }
+    public IIOController getIOController() {
+        return ioController;
+    }
+    public IPluginController getPluginController() {
+        return pluginController;
+    }
+
+    private IAuthenticationController authenticationController = new AuthenticationController();
+    private IIOController ioController = new IOController();
+    private IPluginController pluginController = new PluginController();
+}
