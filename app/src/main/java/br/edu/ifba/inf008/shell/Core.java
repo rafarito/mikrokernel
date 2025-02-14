fe742ff75dc724752f4781a270c1db0933fc7370
Rafael Matos <124383453+rafarito@users.noreply.github.com>
2025-02-14 13:43:20 -0300

    singleton fixes

--- app/src/main/java/br/edu/ifba/inf008/shell/Core.java
+++ app/src/main/java/br/edu/ifba/inf008/shell/Core.java
@@ -7,12 +7,12 @@ public class Core extends ICore
     private Core() {}
 
     public static boolean init() {
-	if (instance != null) {
-	    System.out.println("Fatal error: core is already initialized!");
-	    System.exit(-1);
-	}
+        if (instance != null) {
+            System.out.println("Fatal error: core is already initialized!");
+            System.exit(-1);
+        }
 
-	instance = new Core();
+        instance = new Core();
         UIController.launch(UIController.class);
 
         return true;
@@ -21,24 +21,39 @@ public class Core extends ICore
         return UIController.getInstance();
     }
     public IAuthenticationController getAuthenticationController() {
+        if (authenticationController == null) {
+            authenticationController = new AuthenticationController();
+        }
         return authenticationController;
     }
     public IIOController getIOController() {
+        if(ioController == null) {
+            ioController = new IOController();
+        }
         return ioController;
     }
     public IPluginController getPluginController() {
+        if(pluginController == null) {
+            pluginController = new PluginController();
+        }
         return pluginController;
     }
     public IBookController getBookController() {
+        if(bookController == null) {
+            bookController = new BookController();
+        }
         return bookController;
     }
     public IUserController getUserController() {
+        if(userController == null) {
+            userController = new UserController();
+        }
         return userController;
     }
 
-    private IAuthenticationController authenticationController = new AuthenticationController();
-    private IIOController ioController = new IOController();
-    private IPluginController pluginController = new PluginController();
-    private IBookController bookController = new BookController();
-    private IUserController userController = new UserController();
+    private IAuthenticationController authenticationController;
+    private IIOController ioController;
+    private IPluginController pluginController;
+    private IBookController bookController;
+    private IUserController userController;
 }
