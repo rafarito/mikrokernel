6f68651fccf89c3bcd9e2ca8c3d6f36edccd1de4
Rafael Matos <124383453+rafarito@users.noreply.github.com>
2025-02-19 14:48:57 -0300

    adding bookPlugin

---
+++ plugins/bookPlugin/src/main/java/br/edu/ifba/inf008/plugins/BookPlugin.java
@@ -0,0 +1,19 @@
+package br.edu.ifba.inf008.plugins;
+
+import br.edu.ifba.inf008.interfaces.IPlugin;
+import br.edu.ifba.inf008.interfaces.ICore;
+import br.edu.ifba.inf008.interfaces.IUIController;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+
+public class UserPlugin implements IPlugin
+{
+    public boolean init() {
+        
+    }
+}
